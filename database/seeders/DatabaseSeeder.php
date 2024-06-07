aca7f9a61f9b99a878d37891e85a112221ae9525
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-07 14:39:04 +0200

    Feat → added Technology Migration/Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             TypeSeeder::class,
-            ProjectSeeder::class
+            TechnologySeeder::class,
+            ProjectSeeder::class,
         ]);
     }
 }
--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Http\Traits\Sluggable;
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +15,18 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['html','css', 'bootstrap', 'js', 'vue', 'sql', 'php', 'laravel'];
+
+        foreach ($technologies as $technology_name) {
+            $new_technology = new Technology();
+
+            $new_technology->name = $technology_name;
+            $new_technology->slug = Str::slug($new_technology->name);
+
+            $new_technology->save();
+
+            // $new_technology->posts()->attach($posts_id_coll->random(10)->all());
+        }
+
     }
 }
